ae530941a7ad06d974eed2d31c716ccac630db04
Cristian Alegre Bustos <cnalegrebustos@teco.com.ar>
2023-02-07 12:36:07 -0300

    add api-docs rout

--- dist/server.js
+++ dist/server.js
@@ -9,6 +9,8 @@ const app = express();
 const port = 4000;
 mongoConnect();
 app.get('/', (req, res) => res.send("Si somos tercos como mulas"));
+app.get('/api-docs', (req, res) => res.redirect(`https://studio.apollographql.com/sandbox?endpoint=http%3A%2F%2Flocalhost%3A4000%2Fgraphql`));
+app.get('/api-docs-render', (req, res) => res.redirect(`https://studio.apollographql.com/sandbox?endpoint=https%3A%2F%2Fbusesdatabaseapi.onrender.com%3A4000%2Fgraphql`));
 async function start() {
     const server = new ApolloServer({
         typeDefs: typeDefs,
--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,8 @@ const app = express();
 const port = 4000;
 mongoConnect()
 app.get('/',(req,res)=>res.send("Si somos tercos como mulas"))
- 
+app.get('/api-docs',(req,res)=>res.redirect(`https://studio.apollographql.com/sandbox?endpoint=http%3A%2F%2Flocalhost%3A4000%2Fgraphql`))
+app.get('/api-docs-render',(req,res)=>res.redirect(`https://studio.apollographql.com/sandbox?endpoint=https%3A%2F%2Fbusesdatabaseapi.onrender.com%3A4000%2Fgraphql`))
 async function start() {
   const server = new ApolloServer({
     typeDefs : typeDefs,
@@ -19,8 +20,8 @@ async function start() {
   });
   await server.start();
   server.applyMiddleware({ app });
-  app.get('*',(req,res)=>res.send("404 not found"))
 
+  app.get('*',(req,res)=>res.send("404 not found"))
   app.listen({ port }, () => {
     console.log(
       `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
