0399a8a72b2c8b05391dcb6b23e8b514d9fcc837
Cristian Alegre Bustos <cnalegrebustos@teco.com.ar>
2023-02-18 22:03:48 -0300

    feat: trying add credential in prod

--- src/google-auth.ts
+++ src/google-auth.ts
@@ -4,7 +4,7 @@ import { readFileSync, writeFile } from 'fs';
 import { ParsedQs } from 'qs';
 
 // Lee los datos de las credenciales de Google desde el archivo JSON
-const credentialsPath = path.join(process.cwd(),process.env.NODE_ENV === "development" ? 'credentials.json':"https://busesdatabaseapi.onrender.com/etc/secrets/<filename>");
+const credentialsPath = path.join(process.cwd(),'credentials.json');
 const credentials = JSON.parse(readFileSync(credentialsPath, 'utf8'));
 const REDIRECT_URI = process.env.NODE_ENV === "development" ? 'http://localhost:4000/auth/google/redirect':"https://busesdatabaseapi.onrender.com/auth/google/redirect";
 const SCOPES = ['https://www.googleapis.com/auth/userinfo.profile'];
